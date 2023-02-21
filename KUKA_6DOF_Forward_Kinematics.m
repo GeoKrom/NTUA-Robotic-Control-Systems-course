91cac7f4a9b77d4212511347aa77381580f01ea7
George Krommydas <geokrom98@gmail.com>
2023-02-21 23:25:29 +0200

    Add files via upload

--- KUKA_6DOF_Forward_Kinematics.m
+++ KUKA_6DOF_Forward_Kinematics.m
@@ -1,39 +1,38 @@
-function [End_Effector_Position, A0E] = KUKA_6DOF_Forward_Kinematics(q1,q2,q3,q4,q5,q6,l0,l1,l2,l3,l4,l5,l6,l7)
+function [A0E, End_Effector_Position] = KUKA_6DOF_Forward_Kinematics(q, len)
 
 % KUKA_6DOF_Forward_Kinematics 
 % Input : Joint Position, link length 
 % Output: End Effector Position,Frame of Robot
-q = [q1, q2, q3, q4, q5, q6];
 
 % Matrix from Frame 0 to Frame 1
-A01 = [cos(q(1))  0   -sin(q(1))  l1*cos(q(1));
-       sin(q(1))  0    cos(q(1))  l1*cos(q(1));
-       0         -1    0          l0;
+A01 = [cos(q(1))  0   -sin(q(1))  len(2)*cos(q(1));
+       sin(q(1))  0    cos(q(1))  len(2)*cos(q(1));
+       0         -1    0          len(1);
        0          0    0          1];
 % Matrix from Frame 1 to Frame 2
-A12 = [ sin(q(2))  cos(q(2))   0    l2*sin(q(2));
-       -cos(q(2))  sin(q(2))   0   -l2*cos(q(2));
-        0          0           1    l3;
+A12 = [ sin(q(2))  cos(q(2))   0    len(3)*sin(q(2));
+       -cos(q(2))  sin(q(2))   0   -len(3)*cos(q(2));
+        0          0           1    len(4);
         0          0           0    1];
 % Matrix from Frame 2 to Frame 3
-A23 = [cos(q(3))  0   -sin(q(3))  l4*cos(q(3));
-       sin(q(3))  0    cos(q(3))  l4*sin(q(3));
+A23 = [cos(q(3))  0   -sin(q(3))  len(5)*cos(q(3));
+       sin(q(3))  0    cos(q(3))  len(5)*sin(q(3));
        0         -1    0          0;
        0          0    0          1];
 % Matrix from Frame 3 to Frame 4
 A34 = [cos(q(4))  0    sin(q(4))  0;
        sin(q(4))  0   -cos(q(4))  0;
-       0          1    0          l5;
+       0          1    0          len(6);
        0          0    0          1];
 % Matrix from Frame 4 to Frame 5
 A45 = [cos(q(5))  0   -sin(q(5))  0;
        sin(q(5))  0    cos(q(5))  0;
-       0         -1    0          l6;
+       0         -1    0          len(7);
        0          0    0          1];
 % Matrix from Frame 5 to Frame E
 A5E = [cos(q(6))  -sin(q(6))  0    0;
        sin(q(6))   cos(q(6))  0    0;
-       0           0          1    l7;
+       0           0          1    len(8);
        0           0          0    1];
 
 A0E = A01*A12*A23*A34*A45*A5E;
---
+++ main.m
@@ -0,0 +1,113 @@
+%% Robot Simulation file
+% Parameters of KUKA Robotic Manipulator
+l0 = 0.810;
+l1 = 0.200;
+l2 = 0.600;
+l3 = 0.030;
+l4 = 0.140;
+l5 = 0.550;
+l6 = 0.100;
+l7 = 0.100;
+
+%% Sampling period for kinematic simulation %%
+dt = 0.0001;                            %sec
+
+
+%% Motion Profile - Task (or Cartesian) Space %%
+tf = 10.0;                              % sec
+t = 0:dt:tf;
+
+%% Trajectories %%
+disp("Initialising Desired Task-Space Trajectory (Motion Profile) ...");
+disp(" ");
+% Trajectory from point pA to pB
+x1 = 0.85;% + 0.0085*t.^3 - 0.001275*t.^4 + 0.000051*t.^5; 
+x1_dot = 0.0255*t.^2 - 0.0051*t.^3 + 0.000255*t.^4;
+y1 = 0.13 + 0.0013*t.^3 - 0.000195*t.^4 + 0.0000078*t.^5;
+y1_dot = 0.0039*t.^2 - 0.00078*t.^3 + 0.000039*t.^4;
+z1 = 1.55;
+z1_dot = 0;
+% Trajectory from point pB to pA
+x2 = 1.7 - 0.0085*t.^3 + 0.001275*t.^4 - 0.000051*t.^5;
+x2_dot = -0.0255*t.^2 + 0.0051*t.^3 - 0.000255*t.^4;
+y2 = 0.26 - 0.0013*t.^3 + 0.000195*t.^4 - 0.0000078*t.^5;
+y2_dot = -0.0039*t.^2 + 0.00078*t.^3 - 0.000039*t.^4;
+z2 = 1.55;
+z2_dot = 0;
+
+%% Main Kinematic Loop %%
+disp("Kinematic loop has started");
+disp(" ");
+l = [l0, l1, l2, l3, l4, l5, l6, l7];
+N = numel(t);
+p_d = [x1*ones(1,N); y1; z1*ones(1,N)];
+q_out = zeros(6,N);
+q_out_dot = zeros(6,N);
+p_comp = zeros(N, 3)';
+i = 0;
+
+
+for t_sim = t
+   
+    i = i + 1;
+    q_out(:,i) = KUKA_6DOF_Inverse_Kinematics(p_d(:,i),l);
+    %q_out_dot(:,i) = (q_out(:,i+1) - q_out(:,i))/dt;
+
+end
+i = 0;
+for t_sim = t
+      i = i + 1;
+      [~, p_comp(:,i)] = KUKA_6DOF_Forward_Kinematics(q_out(:,i),l);
+end
+
+
+%% Plots
+figure(1);
+subplot(3,1,1);
+plot(t, p_comp(1,:), 'r-');
+grid;
+hold on;
+plot(t, x1*ones(1,N), 'b.');
+xlabel("time [sec]");
+ylabel("x(t) [m]");
+legend('Computed', 'Desired', 'Location', 'Southwest');
+hold off;
+
+subplot(3,1,2);
+plot(t, p_comp(2,:), 'r-');
+grid;
+hold on;
+plot(t, y1, 'b.');
+xlabel("time [sec]");
+ylabel("y(t) [m]");
+legend('Computed', 'Desired', 'Location', 'Northwest');
+hold off;
+
+subplot(3,1,3);
+plot(t, p_comp(3,:), 'r-');
+grid;
+hold on;
+plot(t, z1*ones(1,N), 'b.');
+xlabel("time [sec]");
+ylabel("z(t) [m]");
+legend('Computed', 'Desired', 'Location', 'Northwest');
+hold off;
+
+figure(2);
+subplot(3,1,1);
+plot(t, q_out(1,:), 'r-');
+grid;
+xlabel("time [sec]");
+ylabel("q_1(t) [rad]");
+
+subplot(3,1,2);
+plot(t, q_out(2,:), 'r-');
+grid;
+xlabel("time [sec]");
+ylabel("q_2(t) [rad]");
+
+subplot(3,1,3);
+plot(t, q_out(3,:), 'r-');
+grid;
+xlabel("time [sec]");
+ylabel("q_3(t) [rad]");
